e5f0d947dcbde3df0631382b641232a605513268
Ben Bancroft <bjbancroft19@gmail.com>
2020-02-24 22:25:19 +0000

    add source maps

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     entry: './src/game.ts',
-    devtool: 'inline-source-map',
+    devtool: 'source-map',
     mode: 'production',
     module: {
         rules: [
